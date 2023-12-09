31326287613186beeeb979f2a1d79ec773817d66
Vlad <Smirnov.ru92@mail.ru>
2023-12-09 11:01:12 +0500

    Deleted unnecessary files.(ui select)

--- src/components/ui/select/select.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Select from 'react-select'
-
-export const CustomSelect = (props) => {
-    return <Select placeholder={'Выберите цвет'} onChange={props.onChange} options={props.options} />
-
-}
